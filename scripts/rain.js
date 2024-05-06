c2303816a15570d276de3ac3b99f0eeaaa13e5b9
k33pup <nikoboga90@gmail.com>
2024-05-06 13:13:33 +0300

    fix

--- scripts/rain.js
+++ scripts/rain.js
@@ -1,17 +1,7 @@
 const rainButton = document.querySelector(".additional__button-rain");
 const rain = document.querySelector(".rain");
 rainButton.addEventListener('click', function () {
-  if ((!localStorage.getItem("rain") || localStorage.getItem("rain") === "none")) {
-    if (window.innerWidth >= 800) {
-      rain.classList.add("rain_open");
-      rain.classList.add("rain_animation");
-    }
-    localStorage.setItem("rain", "raining");
-  } else if (localStorage.getItem("rain") === "raining") {
-    rain.classList.remove("rain_open");
-    rain.classList.remove("rain_animation");
-    localStorage.setItem("rain", "none");
-  }
+  rain.classList.toggle('rain_open');
 })
 
 window.addEventListener('resize', function () {
